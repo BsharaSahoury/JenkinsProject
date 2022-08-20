d491257c07fca103fbb28b3353d377dd8eac0be6
BSHARA <57354656+BsharaSahoury@users.noreply.github.com>
2022-08-21 02:36:09 +0300

    Update C.c

--- C.c
+++ C.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!, I'am from C file.");
+   printf("Hello, World!, I'am from C file.\n");
    return 0;
 }
